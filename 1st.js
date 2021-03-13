802f587ff0cd10b945f37685597a460ea340d0d9
codeclanstudent <sstewart1993@outlook.com>
2021-03-13 12:12:52 +0000

    secrets

--- 1st.js
+++ 1st.js
@@ -1,4 +1,4 @@
-// add something to the start and end of the array using push and unshift
+/////// 1-add something to the start and end of the array using push and unshift //////////
 let myArray = ["a", "b", "c", "d"];
 
 myArray.push("end")
@@ -12,3 +12,18 @@ let myArray1 = ["a", "R", "c", "d"];
 myArray1 = ["start", ...myArray1, "end"]
 
 console.log(myArray1);
+
+////////// 2- create a private variable in JS ////////
+
+function secretVariable(){
+    let private = "super secret Code";
+    return function (){
+        return private
+    }
+}
+
+var getPrivateVariable = secretVariable();
+// this will return the secret
+console.log(getPrivateVariable());
+// this will return an anonymous function
+console.log(secretVariable());
