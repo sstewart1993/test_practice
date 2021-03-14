e7a6e8399b13f2a0664dfadf7a6401082f6986aa
codeclanstudent <sstewart1993@outlook.com>
2021-03-14 15:39:37 +0000

    reverse

--- practice/reverse.js
+++ practice/reverse.js
@@ -2,7 +2,60 @@
 // e.g "hello" = "olleh"
 
 function reverseString(str){
-    
+// .1
+    // const strArr = str.split('');
+    // strArr.reverse();
+    // return strArr.join('');
+
+//////////////////////////////////////////
+// .2
+    // return str
+    //     .split('')
+    //     .reverse()
+    //     .join('');
+
+////////////////////////////////////////
+// .3- decreasing for loop
+// let revString = '';
+// for(let i = str.length -1; i >=0; i --){
+//     revString = revString + str[i];
+// }
+// return revString
+
+////////////////////
+// .4- increasing for loop
+
+// let revString = '';
+// for(let i = 0; i <= str.length -1; i ++){
+//     revString = str[i] + revString;
+// }
+// return revString
+
+////////////////////////////////
+// .5- For-Of loop
+
+// let revString = '';
+// for(let char of str){
+//     revString = char + revString;
+// }
+// return revString
+
+///////////////////////////////
+// .6- For Each method
+
+// let revString = '';
+// str.split('').forEach(function(char){
+//     revString = char + revString;
+// });
+// return revString;
+
+///////////////////////////////////
+// .7- Arrow function same as 6 just using the arrow
+
+let revString = '';
+str.split('').forEach(char => revString = char + revString);
+return revString;
+
 }
 
-// reverseString("hello")
+console.log(reverseString("hello"))
