6df99b4998e2952b0cdca9837cc021490c5721eb
codeclanstudent <sstewart1993@outlook.com>
2021-03-15 16:34:51 +0000

    anagram

---
+++ practice/anagram.js
@@ -0,0 +1,9 @@
+// check if the words are anagrams
+// e.g elbow === below
+// e.g dormitory === dirty room
+
+function isAnagram(str1, str2){
+
+}
+
+console.log(isAnagram("elbow", "below"));
--- practice/chunkArray.js
+++ practice/chunkArray.js
@@ -2,19 +2,71 @@
 // e.g ([1,2,3,4,5,6,7], 3) === ([1,2,3], [4,5,6],[7])
 
 function chunkyArray(arr, len){
+    // .1- using slice method
+
     // init chunkedArray
-    const chunkedArr = []
+    // const chunkedArr = []
     // Set index
-    let i=0;
+    // let i=0;
 
     // loop while index is less than array
-    while(i< arr.length){
-        // slice out of the array and push to the chunkedArr
-        chunkedArr.push(arr.slice(i, i+len))
-        // increment by chunk.length 
-        i += len
-    }
-    return chunkedArr
+    // while(i< arr.length){
+    //     // slice out of the array and push to the chunkedArr
+    //     chunkedArr.push(arr.slice(i, i+len))
+    //     // increment by chunk.length 
+    //     i += len
+    // }
+    // return chunkedArr
+
+
+///////////////////////////////
+    // .2- using the forEach
+
+//     const chunkedArr = [];
+//     // Loop through array
+//     arr.forEach(function(val){
+//         // get last element
+//         const last = chunkedArr[chunkedArr.length -1]
+
+//         // check if last and if last length is equal to the chunk length
+//         if(!last || last.length === len){
+//             chunkedArr.push([val])
+//         }else{
+//             last.push(val)
+//         }
+//     })
+//     return chunkedArr
+// }
 }
 
-console.log(chunkyArray([1,2,3,4,5,6,7], 3));
+// console.log(chunkyArray([1,2,3,4,5,6,7], 3));
+
+
+
+
+/////////////////////////////////////////////////////////////////////////////////
+// e.g  ([1,2,3], [4,5,6],[7]) === [1,2,3,4,5,6,7]
+// flatten an array do the reverse of the chuncked one
+
+
+
+function flattenArray(arrays){
+// .1- use concat
+
+    // return arrays.reduce(function (a,b) {
+    //     return a.concat(b);
+    // })
+
+    ////////////////////////////////////////////
+    // .2- use apply method
+    // return [].concat.apply([],arrays);
+
+    ////////////////////////////////////////
+    // .3- use spread operator
+    return [].concat(...arrays)
+
+
+
+}
+
+console.log(flattenArray([[1,2,3], [4,5,6],[7]]));
