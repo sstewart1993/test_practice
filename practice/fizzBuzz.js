bf8cf25366f0b411f5101fcbb11defaa82c6269e
codeclanstudent <sstewart1993@outlook.com>
2021-03-15 12:33:48 +0000

    max char

---
+++ practice/fizzBuzz.js
@@ -0,0 +1,8 @@
+// create a game of fizzbuzz for 0 to 100
+// 3=fizz, 5=buzz, 15=fizzbuzz
+
+ function fizzBuzz(){
+
+ }
+
+ console.log(fizzBuzz());
--- practice/maxChar.js
+++ practice/maxChar.js
@@ -2,7 +2,25 @@
 // e.g (javascript) = 'a'
 
 function maxCharacter(str){
+    const charMap = {};
+    let maxNum = 0;
+    let maxChar = '';
 
+    str.toLowerCase().split('').forEach(function(char){
+        if(charMap[char]){
+            charMap[char]++
+        } else{
+            charMap[char] = 1
+        }
+    })
+
+    for(let char in charMap){
+        if(charMap[char] > maxNum){
+            maxNum = charMap[char];
+            maxChar = char
+        }
+    }
+    return maxNum + ' x ' + maxChar
 }
 
 console.log(maxCharacter("javascript"));
