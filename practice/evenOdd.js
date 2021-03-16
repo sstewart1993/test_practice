12a41c5bbb84ad2d4ea570012ddb2fd622c87954
codeclanstudent <sstewart1993@outlook.com>
2021-03-16 16:10:31 +0000

    done

--- practice/evenOdd.js
+++ practice/evenOdd.js
@@ -1,7 +1,28 @@
 // take an array and return the sum of the even numbers and the odd
 // e.g ([50, 60, 60, 45, 71]) == [170, 116]
 
+// function evenOddSums(arr){
+//     let evenSum = 0
+//     let oddSum = 0
+
+//     arr.forEach(num => (num % 2 === 0 ? (evenSum += num) : (oddSum += num)));
+
+//     return [evenSum, oddSum]
+// }
+
 function evenOddSums(arr){
+let arrEven = 0
+let arrOdd = 0
+
+
+for(i=0; i <= arr.length -1; i++){
+    if(arr[i] % 2 === 0){
+        arrEven += arr[i]
+    }else{
+        arrOdd += arr[i]
+    }}
+
+return [arrEven, arrOdd]
 
 }
 
