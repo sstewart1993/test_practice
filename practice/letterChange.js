13c7df10f40a64b769be90d109ab56b2463c81f9
codeclanstudent <sstewart1993@outlook.com>
2021-03-16 11:35:50 +0000

    3

--- practice/letterChange.js
+++ practice/letterChange.js
@@ -2,7 +2,19 @@
 // capitalize vowels (a,e,i,o,u)
 
 function letterChanges(str){
-    
+    let newStr = str.toLowerCase().replace(/[a-z]/gi, function(char){
+        if(char === 'z' || char === 'Z'){
+            return 'a'
+        }else{
+            return String.fromCharCode(char.charCodeAt() + 1);
+        }
+    });
+
+    newStr = newStr.replace(/a|e|i|o|u/gi, function(vowel){
+        return vowel.toUpperCase();
+    })
+
+    return newStr
 }
 
-console.log(letterChanges("Hello World"));
+console.log(letterChanges("Hello Worldz"));
